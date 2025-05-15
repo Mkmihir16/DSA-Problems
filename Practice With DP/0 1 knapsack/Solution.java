3f11137bc2d5e41d09c33e213f7bd137891e33bb
Mihir Kalbende <mkmihir16@gmail.com>
2025-05-15 11:57:26 +0530

    code added for tabulation of 0 1 knasack

--- Practice With DP/0 1 knapsack/Solution.java
+++ Practice With DP/0 1 knapsack/Solution.java
@@ -32,7 +32,44 @@ public class Solution {
             return dp[i][W];
         }
     }
-    public static void main(String[] args) {
+    public static  int findCost_using_tabulation(int[] val,int[] wt,int W){
+
+        int n=val.length;
+        int dp[][]=new int[n+1][W+1];
+
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
+        }
+        for(int j=0;j<=W;j++){
+            dp[0][j]=0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=W;j++){
+                int v=val[i-1];
+                int w=wt[i-1];
+                if(j>=w){
+                    //including condition
+                    int profit1=v+dp[i-1][j-w];
+                    //excluding condition
+                    int profit2=dp[i-1][j];
+                    dp[i][j]=Math.max(profit1,profit2);
+                }
+                else{
+                    int profit=dp[i-1][j];
+                    dp[i][j]=profit;
+                }
+            }
+        }
+        System.out.println("DP Table:");
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=W;j++){
+                System.out.print(dp[i][j]+" ");
+            }
+            System.out.println();
+        }
+        return dp[n][W];
+    }
+        public static void main(String[] args) {
         int val[]={15,14,10,45,30};
         int wt[]={2,5,1,3,4};
         int W=7;
@@ -46,5 +83,7 @@ public class Solution {
         }
         //using memoization
         System.out.println( findCost_using_memoization(0,val,wt,W,dp)); //using memoization
+        //using tabulation
+        System.out.println(findCost_using_tabulation(val,wt,W)); //using tabulation
     }
 }
