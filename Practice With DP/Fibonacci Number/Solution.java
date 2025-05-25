69df89ea72eac7877b11e03ae0c9d2ccd657314c
Mihir Kalbende <mkmihir16@gmail.com>
2025-05-25 17:18:42 +0530

    added fibonaaci with tabulation

--- Practice With DP/Fibonacci Number/Solution.java
+++ Practice With DP/Fibonacci Number/Solution.java
@@ -18,10 +18,21 @@ public class Solution{
         dp[n]=fibonacci_with_memoization(n-1,dp)+fibonacci_with_memoization(n-2,dp);
         return dp[n];
     }
+    public static int fibonacci_with_tabulation(int n){
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
     public static void main(String[] args){
         int n=15;
         int[] dp =new int[n+1];
-        System.out.println(fibonacci(n));
-        System.out.println(fibonacci_with_memoization(n,dp));
+        System.out.println("Fibonacci with recursion "+fibonacci(n));
+        System.out.println("Fibonacci with memoization "+fibonacci_with_memoization(n,dp));
+        System.out.println("Fibonacci with tabulation "+fibonacci_with_tabulation(n));
     }
 }
