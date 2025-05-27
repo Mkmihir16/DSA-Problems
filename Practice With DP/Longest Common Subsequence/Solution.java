19cb427a2c0fca5093222ff7672456173f034aea
Mihir Kalbende <mkmihir16@gmail.com>
2025-05-27 13:30:53 +0530

    added lcs using tabulation

--- Practice With DP/Longest Common Subsequence/Solution.java
+++ Practice With DP/Longest Common Subsequence/Solution.java
@@ -32,6 +32,29 @@ public class Solution {
         }
         return dp[n][m];
     }
+    public static int lcs_using_tabulation(String s1,String s2){
+        int n=s1.length();
+        int m=s2.length();
+        int dp[][]=new int[n+1][m+1];
+
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0; // If s2 is empty, LCS length is 0
+        }
+        for(int i=0;i<=m;i++){
+            dp[0][i]=0; // If s1 is empty, LCS length is 0
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1]; // If characters match, add 1 to the LCS length
+                } else {
+                    dp[i][j]=Math.max(dp[i-1][j], dp[i][j-1]); // If characters don't match, take the max of excluding either character
+                }
+            }
+        }
+        return dp[n][m]; // The length of the longest common subsequence
+    }
     public static void main(String[] args) {
         String s1="abcde";
         String s2="ace";
@@ -45,6 +68,6 @@ public class Solution {
         }
         System.out.println("Length of Longest Common Subsequence is: " + lcs_using_recursion(s1, s2, n-1, m-1));
         System.out.println("Length of Longest Common Subsequence using DP is: " + lcs_using_memoization(s1, s2, n-1, m-1, dp));  
-
+        System.out.println("Length of Longest Common Subsequence using Tabulation is: " + lcs_using_tabulation(s1, s2));
     }    
 }
