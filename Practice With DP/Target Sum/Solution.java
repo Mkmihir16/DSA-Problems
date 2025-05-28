306c7b68bb08f2e24536ba6b37855ea56eb8856d
Mihir Kalbende <mkmihir16@gmail.com>
2025-05-28 13:12:32 +0530

    added target sum using recursion

--- Practice With DP/Target Sum/Solution.java
+++ Practice With DP/Target Sum/Solution.java
@@ -1,5 +1,21 @@
 
 public class Solution {
+    public static boolean  findTarget_using_recursion(int i,int[] nums,int target){
+        if(target==0){
+            return true;
+        }
+        if(i==nums.length){
+            return false;
+        }
+        if(nums[i]<=target){
+            boolean res1=findTarget_using_recursion(i+1, nums, target-nums[i]);
+            boolean res2=findTarget_using_recursion(i+1,nums,target);
+            return res1  || res2;
+        }
+        else{
+            return findTarget_using_recursion(i+1, nums, target);
+        }
+    }
     public static boolean findTargetSumWays(int[] nums, int target) {
         int n = nums.length;
         boolean[][] dp = new boolean[n + 1][target + 1];
@@ -27,10 +43,18 @@ public class Solution {
         }
         return dp[n][target];
     }
-
     public static void main(String[] args) {
         int[] nums = { 1, 2, 3 };
         int target = 4;
-        System.out.println("Target Sum: " + findTargetSumWays(nums, target));
+        boolean dp[][]=new boolean[nums.length+1][target+1];
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
+                dp[i][j]=false;
+            }
+        }
+        System.out.println("Target Sum using memoization "+ findTarget_using_memoization(0,dp,nums,target));
+        System.out.println("Target Sum using tabulation: " + findTargetSumWays(nums, target));
+        System.out.println("Target Sum using recursioon: " + findTarget_using_recursion(0,nums, target));
+
     }
 }
