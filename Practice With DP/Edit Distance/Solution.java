36f42535f820cd1c1e88721f80fdec818684512e
Mihir Kalbende <mkmihir16@gmail.com>
2025-05-30 11:09:56 +0530

    added new code

--- Practice With DP/Edit Distance/Solution.java
+++ Practice With DP/Edit Distance/Solution.java
@@ -26,6 +26,13 @@ public class Solution {
                 }
             }
         }
+
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
+                System.out.print(dp[i][j]+" ");
+            }
+            System.out.println();
+        }
         return dp[n][m];
     }
     public static void main(String[] args) {
