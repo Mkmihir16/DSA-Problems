0785e312a26519d31cd4b63df2a453e3935fbc8f
Mihir Kalbende <mkmihir16@gmail.com>
2025-06-01 11:26:15 +0530

    add memoization code

--- Practice With DP/Catalans Number/Solution.java
+++ Practice With DP/Catalans Number/Solution.java
@@ -1,16 +1,35 @@
+import java.util.Arrays;
+
 public class Solution {
-    public static int fun(int n){
+    public static int recursion(int n){
         if(n==0 || n==1){
             return 1;
         }
         int sum=0;
         for(int i=0;i<n;i++){
-            sum+=fun(i)*fun(n-i-1);
+            sum+=recursion(i)*recursion(n-i-1);
         }
         return sum;
     }
+    public static int memoization(int n,int[] dp){
+        if(n==0 || n==1){
+            return 1;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=memoization(i,dp)*memoization(n-i-1,dp);
+        }
+        dp[n]=sum;
+        return sum;
+    }
     public static void main(String[] args) {
-        int n=3;
-        System.out.println(fun(3));
+        int n=5;
+        System.out.println(recursion(n));
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        System.out.println(memoization(n,dp));
     }
 }
