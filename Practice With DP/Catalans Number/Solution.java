46c4fc2acd91a252c9e1c813e94702d6502038b5
Mihir Kalbende <mkmihir16@gmail.com>
2025-06-01 12:37:38 +0530

    added the tabulation of catalan code

--- Practice With DP/Catalans Number/Solution.java
+++ Practice With DP/Catalans Number/Solution.java
@@ -25,11 +25,25 @@ public class Solution {
         dp[n]=sum;
         return sum;
     }
+    public static int tabulation(int n){
+        int dp[]=new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            int ans=0;
+            for(int j=0;j<i;j++){
+                ans+=dp[j]*dp[i-j-1];
+            }
+            dp[i]=ans;
+        }
+        return dp[n];
+    }
     public static void main(String[] args) {
-        int n=5;
-        System.out.println(recursion(n));
+        int n=10;
+        // System.out.println(recursion(n));
         int dp[]=new int[n+1];
         Arrays.fill(dp,-1);
-        System.out.println(memoization(n,dp));
+        System.out.println("catalan number using memoization "+memoization(n,dp));
+        System.out.println("catalan number using tabulation "+tabulation(n));
     }
 }
